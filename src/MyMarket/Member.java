a6f78abe7cc72f6a5f8759d8e1538d27bf3ccf03
Iamquman <2323558231@qq.com>
2019-11-28 09:45:21 +0800

    遍历文件判断手机号是否已存在若存在不能插入

--- src/MyMarket/Member.java
+++ src/MyMarket/Member.java
@@ -1,10 +1,14 @@
 package MyMarket;
+
+import java.time.Instant;
+import java.util.InputMismatchException;
+
 //会员类
 public class Member {
     //姓名，手机号，密码，卡号，积分，开卡日期
     private String name;
     private String phone;
-    private int mima;
+    private String mima;
     private int kahao;
     private int jifen;
     private String riqi;
@@ -13,11 +17,11 @@ public class Member {
         return name;
     }
 
-    public long getPhone() {
+    public String getPhone() {
         return phone;
     }
 
-    public int getMima() {
+    public String getMima() {
         return mima;
     }
 
@@ -38,19 +42,31 @@ public class Member {
     }
 
     public boolean setPhone(String phone) {
-        
-        if(phone.length()==11){
-            this.phone=phone;
+        if(phone.length()==11 && testPhone(phone)){
             return true;
-
         }else{
-
+            return false;
         }
     }
 
-    public boolean setMima(int mima) {
-        if(mima>=6) {
-            this.mima = mima;
+    public boolean testPhone(String phone) {
+        boolean flag=true;
+        for (int i = 0; i < phone.length(); i++) {
+            char[] p = new char[phone.length()];
+            p[i] = phone.charAt(i);
+            if (!Character.isDigit(p[i])) {  //判断字符是否为纯数字的方法
+                flag=false;
+                break;
+            } else {
+                flag= true;
+                this.phone=phone;
+            }
+        }
+        return flag;
+    }
+
+    public boolean setMima(String mima) {
+        if(mima.length()>=6){
             return true;
         }else{
             return false;
--- src/MyMarket/MemberDao.java
+++ src/MyMarket/MemberDao.java
@@ -1,6 +1,12 @@
 package MyMarket;
 
+import javax.swing.*;
+import javax.xml.crypto.Data;
+import java.io.*;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 //操作类
@@ -8,40 +14,122 @@ public class MemberDao {
     static Scanner sc= new Scanner(System.in);
     ArrayList<Member> member=new ArrayList();
 
+    public static void testFile(Member me){
+        ArrayList<String> lines = new ArrayList<>();
+        File f=new File(".\\files\\Member.txt");
+        try {
+            FileReader fr=new FileReader(f);
+            BufferedReader br= new BufferedReader(fr);
+            String line = br.readLine();
+            while(null!=line){
+                lines.add(line);
+                line=br.readLine();
+            }
+            br.close();
+            fr.close();
+
+            boolean flag=true;
+            for(String l:lines){
+                String[] s=l.split(",");
+                if(s[1].equals(me.getPhone())){
+                    System.out.println("该手机号已被开通！");
+                    flag=false;
+                    break;
+                }
+;            }
+            if(flag){
+                fileMem(me);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public static void fileMem(Member me){
+        File f=new File(".\\files\\Member.txt");
+        String s=me.getName()+","+me.getPhone()+","+me.getKahao()+","+me.getRiqi();
+        try {
+            FileOutputStream fos=new FileOutputStream(f,true);
+            byte[] b=s.getBytes();
+            fos.write(b);
+            String n="\n";
+            byte[] n1=n.getBytes();
+            fos.write(n1);
+            fos.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void kaiKa(){
         Member me=new Member();
         System.out.print("请输入注册姓名：");
         String name=sc.next();
         me.setName(name);
         System.out.print("请输入手机号：");
-        String phone=sc.next();
-        me.setPhone(phone);
-        System.out.println("请输入注册密码：");
-        int mima=sc.nextInt();
-        me.setMima(mima);
-
+        do {
+            String phone = sc.next();
+            if (!me.setPhone(phone)) {
+                System.out.println("输入有误，请重新输入：");
+                continue;
+            }else{
+                me.setPhone(phone);
+                break;
+            }
+        }while (true);
+        System.out.print("请输入注册密码(不少于六位)：");
+        do {
+            String mima = sc.next();
+            if(!me.setMima(mima)){
+                System.out.println("输入不符合，请重新输入：");
+                continue;
+            }else {
+                break;
+            }
+        }while(true);
+        me.setKahao(mumber());
+        me.setJifen(1000+me.getJifen());
+        me.setRiqi(time());
+        testFile(me);
     }
+
+    public static String time(){
+        Date date=new Date();
+        SimpleDateFormat sdf=new SimpleDateFormat("yy-MM-dd:hh");
+        return sdf.format(date);
+    }
+
+    public static int mumber(){
+        return (int)(Math.random()*100000000+1);
+    }
+
     public static void start(){
         do {
-        int choose = sc.nextInt();
+        String choose = sc.next();
             switch (choose) {
-                case 1:
+                case "1":
                     //积分累计
                     break;
-                case 2:
+                case "2":
                     //积分兑换
                     break;
-                case 3:
+                case "3":
                     //查询剩余积分
                     break;
-                case 4:
+                case "4":
                     //开卡
                     kaiKa();
                     break;
-                case 5:
+                case "5":
                     //修改密码
                     break;
-                case 6:
+                case "6":
                     //退出
                     break;
                 default:
@@ -66,7 +154,7 @@ public class MemberDao {
         System.out.println("*************************************");
         System.out.println("请选择(1-6)：");
     }
-    public static void main(String[] args){
+    public static void main(String[] args) {
         caiDan();
         start();
     }
